35b1210c9b96b15f2734fe2bb67ff9f8f50141fe
pedro.perini <pedro.perini@dgtallab.com.br>
2025-05-19 20:06:42 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/dslist/DslistBackApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistBackApplication.class, args);
+    }
+
+}
